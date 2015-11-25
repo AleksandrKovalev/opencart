62f0cc1355b2aa151feb0f04c7a9cc90e731e90c
Aleksandr <kovalev.2014.aleksandr@gmail.com>
2015-11-25 21:39:07 +0200

    init

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart.lh/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart.lh/');
+
+// DIR
+define('DIR_APPLICATION', 'Z:/home/opencart.lh/www/catalog/');
+define('DIR_SYSTEM', 'Z:/home/opencart.lh/www/system/');
+define('DIR_LANGUAGE', 'Z:/home/opencart.lh/www/catalog/language/');
+define('DIR_TEMPLATE', 'Z:/home/opencart.lh/www/catalog/view/theme/');
+define('DIR_CONFIG', 'Z:/home/opencart.lh/www/system/config/');
+define('DIR_IMAGE', 'Z:/home/opencart.lh/www/image/');
+define('DIR_CACHE', 'Z:/home/opencart.lh/www/system/storage/cache/');
+define('DIR_DOWNLOAD', 'Z:/home/opencart.lh/www/system/storage/download/');
+define('DIR_LOGS', 'Z:/home/opencart.lh/www/system/storage/logs/');
+define('DIR_MODIFICATION', 'Z:/home/opencart.lh/www/system/storage/modification/');
+define('DIR_UPLOAD', 'Z:/home/opencart.lh/www/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'developer_3');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
